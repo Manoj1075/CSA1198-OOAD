434cbd29b331541ac1c41437b9040cf7104d13fd
Manoj1075 <124116570+Manoj1075@users.noreply.github.com>
2023-08-31 13:46:06 +0530

    Q35

---
+++ Beautycian.cpp
@@ -0,0 +1,26 @@
+#include "Beautycian.h"
+
+// Constructors/Destructors
+//  
+
+Beautycian::Beautycian () {
+initAttributes();
+}
+
+Beautycian::~Beautycian () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void Beautycian::initAttributes () {
+}
+
---
+++ Billing_officer.cpp
@@ -0,0 +1,26 @@
+#include "Billing_officer.h"
+
+// Constructors/Destructors
+//  
+
+Billing_officer::Billing_officer () {
+initAttributes();
+}
+
+Billing_officer::~Billing_officer () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void Billing_officer::initAttributes () {
+}
+
